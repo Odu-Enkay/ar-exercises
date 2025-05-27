4ee6bf185de43eea6f7159f5271919cab1be44a1
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-27 13:18:12 -0600

    implements ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total revenue for the entire company is $#{Store.sum(:annual_revenue)}"
+puts "Average Annual revernue for the entire company is $#{Store.average(:annual_revenue)}"
+
+aboveOneMil = Store.where("annual_revenue >= ?", 1000000).count
+puts "There are #{aboveOneMil} stores generating $1M or more in annual sales."
+
+
+
